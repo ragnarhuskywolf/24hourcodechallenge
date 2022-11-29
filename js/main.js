0f905a1e5d28266b19006127850ca3b2ac7a6497
Lester Magdura <lester@matterdesign.com.au>
2022-11-29 17:49:18 +0800

    Added slick js and assets + update index.html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log('test');
+ $(document).ready(function(){
+      $('#hero-carousel').slick({
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        dots: true,
+      });
+    });
